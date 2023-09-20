c34e38292e678b3d5a65efc9fb410182347d5fbc
JuanCristiano <jcristiano@hotmail.com.ar>
2023-09-20 14:08:32 -0300

    se creo footer, navbar, toolbar

---
+++ src/app/foot/foot.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-foot',
+  template: `
+    <p>
+      foot works!
+    </p>
+  `,
+  styles: [
+    `
+    foot {
+      background-color: olive;
+    }
+    `
+  ]
+})
+export class FootComponent {
+
+}
